bfed8b634bf7e38f9f87f6d6f4b1595e7b5467d8
Michel Perez <melodisofia.48@gmail.com>
2024-03-05 22:42:58 -0500

    Agregando array a partir de cadena de texto

--- Index.js
+++ Index.js
@@ -1,3 +1,8 @@
 let arr = new Array ("Aguacate", "Gato", "Conejo", "pelusa", "josé", 4, 8, 3);
+
 let numeros = Array.from({length:10},(_, index) => index + 1)
-let arrayPelusas = Array(5).fill("pelusa");
+
+
+let texto = "A la vibora vibora de la mar";
+let arrayTexto = texto.split(' ');
+console.log(arrayTexto);
