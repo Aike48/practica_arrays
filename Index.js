e5fc9bc7a44636d76125bf9a80e99c163800d8fe
Michel Perez <melodisofia.48@gmail.com>
2024-03-05 23:26:40 -0500

    Creando funcion que genera valores dinamicos

--- Index.js
+++ Index.js
@@ -11,3 +11,16 @@ let arrayDuplicados = [1, 2, 2, 3, 3, 4, 5, 5];
 let setUnico = new Set(arrayDuplicados);
 let arrayUnico = [...setUnico];
 console.log(arrayUnico); 
+
+function generarValores(n) {
+    let array = [];
+    for (let i = 1; i <= n; i++) {
+      array.push(i * 2); 
+    }
+    return array;
+  }
+  
+  const arrayDinamico = generarValores(5);
+  console.log(arrayDinamico); 
+  
+  
