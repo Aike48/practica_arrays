7bf75a70161b53ea04bc4f950450c4c194290a46
Michel Perez <melodisofia.48@gmail.com>
2024-03-05 22:30:29 -0500

    Agregando array usando repetición de valores Fill

--- Index.js
+++ Index.js
@@ -1,2 +1,3 @@
 let arr = new Array ("Aguacate", "Gato", "Conejo", "pelusa", "josé", 4, 8, 3);
 let numeros = Array.from({length:10},(_, index) => index + 1)
+let arrayPelusas = Array(5).fill("pelusa");
