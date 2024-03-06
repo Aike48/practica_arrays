deb6c2e43e3fe9bdb733d510af01f56f0594ad1f
Michel Perez <melodisofia.48@gmail.com>
2024-03-05 22:49:29 -0500

    Combinando arrays de numeros y texto (son los nombres de mis arrays)

--- Index.js
+++ Index.js
@@ -6,3 +6,6 @@ let numeros = Array.from({length:10},(_, index) => index + 1)
 let texto = "A la vibora vibora de la mar";
 let arrayTexto = texto.split(' ');
 console.log(arrayTexto);
+
+let ArrayCombinados = [...numeros, ...texto];
+console.log(ArrayCombinados);
