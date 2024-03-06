44a501192152b2fdf31b9b1d9a89495be05a38f1
Michel Perez <melodisofia.48@gmail.com>
2024-03-05 23:09:34 -0500

    Creando array con valores unicos usando metodo set y operador spread

--- Index.js
+++ Index.js
@@ -6,3 +6,8 @@ let numeros = Array.from({length:10},(_, index) => index + 1)
 let texto = "A la vibora vibora de la mar";
 let arrayTexto = texto.split(' ');
 console.log(arrayTexto);
+
+let arrayDuplicados = [1, 2, 2, 3, 3, 4, 5, 5];
+let setUnico = new Set(arrayDuplicados);
+let arrayUnico = [...setUnico];
+console.log(arrayUnico); 
